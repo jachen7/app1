412cb2f6c59c871324290c1dfd803d11fe630bd1
jachen7 <jachen@cht.com.tw>
2018-01-09 15:39:19 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,8 +1,8 @@
 <?php
-  echo "hello, i am jachen"
-  //$json_str=file_get_contents('php://input'); //接收RESUEST的BODY
-  //$json_obj=json_decode($json_str); //轉JSON格式
-  //$myfile=fopen("log.txt","w+") or die("unable to open file!"); //log.txt接收訊息
-  //fwrite($myfile, "\xEF\xBB\xBf".$json_str);
-  //fclose($myfile);
+  //echo "hello, i am jachen"
+  $json_str=file_get_contents('php://input'); //接收RESUEST的BODY
+  $json_obj=json_decode($json_str); //轉JSON格式
+  $myfile=fopen("log.txt","w+") or die("unable to open file!"); //log.txt接收訊息
+  fwrite($myfile, "\xEF\xBB\xBf".$json_str);
+  fclose($myfile);
 ?>
